9b7c74e8edbf21ad7c6bd0ed9aeeeae49b54494d
kishore <kishore@leaguex.com>
2024-03-06 23:17:52 +0530

    Removed Color code

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -50,7 +50,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     position: "relative",
     zIndex: 1,
-    backgroundColor: "red",
   },
   headerTextContainer: {
     position: "relative",
