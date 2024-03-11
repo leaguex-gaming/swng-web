c67b303f24f10d45b80a9420a0eae0d8391dd272
kishore <kishore@leaguex.com>
2024-03-11 12:10:45 +0530

    handled parallel route

--- src/utils/RequireAuth.js
+++ src/utils/RequireAuth.js
@@ -11,7 +11,7 @@ export default function RequireAuth({ children }) {
   useEffect(() => {
     if (!accessToken) push("/");
     else push("/feed");
-  }, []);
+  }, [accessToken]);
 
   return <>{children}</>;
 }
