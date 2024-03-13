317ae87a9c03b8a63cf96afaa1b0e6a99c0765c5
kishore <kishore@leaguex.com>
2024-03-13 15:39:39 +0530

    changed url

--- src/app/post/[postId]/page.js
+++ src/app/post/[postId]/page.js
@@ -1,27 +1,26 @@
 import PostInfo from "@/components/community/PostInfo";
-import { cookies } from "next/headers";
 
 export async function generateMetadata({ params, searchParams }, parent) {
   // read route params
   const id = params.postId;
 
-  const url = `https://966ievwykg.execute-api.ap-south-1.amazonaws.com/development/api/v1/sportsgram/posts?post_id=${id}`;
+  let url = `https://l1z6btl2l9.execute-api.ap-south-1.amazonaws.com/production/api/v1/sportsgram/posts?post_id=${id}`;
 
   const res = await fetch(url).then((res) => res.json());
 
   return {
-    title: `Swng photos and videos . ${res.posts.topic.name}`,
-    description: `Swng photos and videos .  ${res.posts.content}`,
-    keywords: `Swng, Sports, Sport Feeds, ${res.posts.topic.name}, ${res.posts.subtopic.name}`,
+    title: `Swng photos and videos . ${res?.posts?.topic?.name}`,
+    description: `Swng photos and videos .  ${res?.posts?.content}`,
+    keywords: `Swng, Sports, Sport Feeds, ${res?.posts?.topic.name}, ${res?.posts?.subtopic?.name}`,
 
     openGraph: {
-      title: `Swng photos and videos . ${res.posts.topic.name} `,
-      description: `Swng photos and videos .  ${res.posts.content}`,
+      title: `Swng photos and videos . ${res?.posts?.topic?.name} `,
+      description: `Swng photos and videos .  ${res?.posts?.content}`,
       type: "article",
       url: "https://www.swng.fan/",
       images: [
         {
-          url: res.posts.media_url,
+          url: res?.posts?.media_url,
         },
       ],
       site_name: "Swng",
