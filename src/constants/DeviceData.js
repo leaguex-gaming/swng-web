09cbba5bb0edd22c112b3abcfdb70fc21d299c29
kishore <kishore@leaguex.com>
2024-03-18 11:26:31 +0530

    fixed window width

--- src/constants/DeviceData.js
+++ src/constants/DeviceData.js
@@ -3,5 +3,10 @@
 import { Platform } from "react-native-web";
 
 export const PlatformOS = Platform.OS;
-export const windowMaxWidth = window.innerWidth > 500 ? 500 : window.innerWidth;
+export let windowMaxWidth = 500;
+
+if (typeof window !== "undefined") {
+  windowMaxWidth = window.innerWidth > 500 ? 500 : window.innerWidth;
+}
+
 export const windowMaxHeight = "100vh";
