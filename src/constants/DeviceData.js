4e3a99441f69ba5ca8b69e5af12af2bf738b5d66
kishore <kishore@leaguex.com>
2024-03-18 11:09:03 +0530

    fixed responsive

--- src/constants/DeviceData.js
+++ src/constants/DeviceData.js
@@ -3,5 +3,5 @@
 import { Platform } from "react-native-web";
 
 export const PlatformOS = Platform.OS;
-export const windowMaxWidth = 500;
+export const windowMaxWidth = window.innerWidth > 500 ? 500 : window.innerWidth;
 export const windowMaxHeight = "100vh";
